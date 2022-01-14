29466d026c8cbdca1191fae031050ef3fd32e8f1
Joel Huang <miniginter@gmail.com>
2022-01-15 02:35:00 +1100

    Progress until tmr

--- hard.py
+++ hard.py
@@ -18,6 +18,8 @@ we will also consider many other criteria.
 """
 import json
 
+import re
+
 # NOTE: DO NOT EDIT conditions.json
 with open("./conditions.json") as f:
     CONDITIONS = json.load(f)
@@ -39,24 +41,41 @@ def is_unlocked(courses_list, target_course):
 
     for condition in CONDITIONS:
         if condition == target_course:
-            if CONDITIONS[target_course] == "":
+            # required_list = CONDITIONS[target_course].split('AND', flags=re.IGNORECASE)
+            required_list = re.split("AND", CONDITIONS[target_course], flags=re.IGNORECASE)
+            print(required_list)
+
+            ################################################################
+            # Basic checks
+            if len(CONDITIONS[target_course]) == 0:
                 return True
-            # elif len(CONDITIONS[target_course]) == 8:
-            #     for course in courses_list:
-            #         if course in CONDITIONS[target_course]:
-            #             return True
-            elif "and" not in CONDITIONS[target_course]:
+            elif len(CONDITIONS[target_course]) == 8:
                 for course in courses_list:
                     if course in CONDITIONS[target_course]:
                         return True
+            ################################################################
+            
+            elif "and" not in CONDITIONS[target_course] and "or" in CONDITIONS[target_course]:
+                for course in courses_list:
+                    if course in CONDITIONS[target_course]:
+                        return True
+
             # print(CONDITIONS[target_course])
     
     return False
 
+def handle_basic():
+    pass
+
+
 if __name__ == "__main__":
-    print(is_unlocked([], "COMP1511"))
-    print(is_unlocked([], "COMP9301"))
-    print(is_unlocked(["MATH1081"], "COMP3153"))
+    print(is_unlocked([], "COMP1511")) # true 
+    print(is_unlocked([], "COMP9301")) # false 
+    print(is_unlocked(["MATH1081"], "COMP3153")) # true 
+    print(is_unlocked([], "COMP1521")) 
+    print(is_unlocked([], "COMP2511")) 
+    print(is_unlocked([], "COMP3151")) 
+    print(is_unlocked([], "COMP3900")) 
 
 
 
